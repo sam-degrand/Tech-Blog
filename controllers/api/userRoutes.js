da832abf8a2789affefdcb47786c2ae8c35b3203
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-07-17 23:43:13 -0400

    changed "project" to "post", deleted a thing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  try{
+      const userData = await User.create({
+          name: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+        });
+      console.log(userData);
+      req.session.save(() => {
+          req.session.user_id = userData.id; 
+          req.session.logged_in = true; 
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
+          res.status(200).json(userData);
+      });
+  } catch(err) {
+      console.log(err);
+      res.status(400).json(err); 
   }
-});
+})
 
 router.post('/login', async (req, res) => {
   try {
