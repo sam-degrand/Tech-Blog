c231a1a22076fe306dcb8eadc9ccbf11298f202c
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-07-17 22:52:22 -0400

    grabbed models from mini project for convience

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,37 +1,55 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', async (req, res) => {
-  try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
+router.post('/', withAuth, async(req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            post_description: req.body.description,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    }catch(err){
+        console.log(err);
+        res.status(400).json(err);
     }
-
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if(!postData) {
+            res.status(404).json({message: 'No post found with this id!'});
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+    try{
+        const updatedPost = {title: req.body.newTitle, post_description: req.body.newDescription};
+        const postData = await Post.update(updatedPost, {
+            where: {
+                id: req.params.id,
+            }
+        })
+        console.log(postData);
+        res.status(200).json(postData);
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
